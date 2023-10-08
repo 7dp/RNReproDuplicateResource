6cfd79df027d7c891c7ffa7d745d57377ca36bed
Wicak Radityo <wicakradityo778@gmail.com>
2023-10-08 21:50:39 +0700

    feat: install modal-dropdown and network-logger

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
+import {startNetworkLogging} from 'react-native-network-logger';
 
+startNetworkLogging();
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
