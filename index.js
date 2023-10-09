d9ad49ee35c2b3496d201af925d0893c20bc71ce
Wicak Radityo <wicakradityo778@gmail.com>
2023-10-09 14:01:59 +0700

    feat: remove network-logger

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
-import {startNetworkLogging} from 'react-native-network-logger';
 
-startNetworkLogging();
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
